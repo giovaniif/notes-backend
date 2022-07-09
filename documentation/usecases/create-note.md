f8ad6b4df992f5ecd47bcf9abe6172f726704bb7
Giovani Farias <riccog.25@gmail.com>
2022-07-09 01:09:12 -0300

    test: ensure createNote calls repository correctly

---
+++ documentation/usecases/create-note.md
@@ -0,0 +1,4 @@
+### Create Note
+
+- A note has a title and content
+- Should not be able to create a note without a title and a content
---
+++ tests/create-note.spec.ts
@@ -0,0 +1,38 @@
+import { mock, MockProxy } from "jest-mock-extended"
+
+namespace CreateNoteRepository {
+  export type Input = { title: string, content: string }
+}
+interface CreateNoteRepository {
+  create: (input: CreateNoteRepository.Input) => Promise<void>
+}
+type Input = { title: string, content: string }
+type CreateNote = (input: Input) => Promise<void>
+const setupCreateNote = (createNoteRepository: CreateNoteRepository): CreateNote => {
+  return async ({ content, title }) => {
+    await createNoteRepository.create({ content, title })
+  }
+}
+
+describe('Create Note', () => {
+  let sut: CreateNote
+  let title: string
+  let content: string
+  let createNoteRepository: MockProxy<CreateNoteRepository>
+
+  beforeAll(() => {
+    title = 'any_title'
+    content = 'any_content'
+    createNoteRepository = mock()
+  })
+
+  beforeEach(() => {
+    sut = setupCreateNote(createNoteRepository)
+  })
+
+  it('should call createNoteRepository with correct title and content', async () => {
+    await sut({ title, content })
+
+    expect(createNoteRepository.create).toHaveBeenCalledWith({ title, content })
+  })
+})
