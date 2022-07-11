c398c1747930a99a0150245df72b3789188bd92d
Giovani Farias <riccog.25@gmail.com>
2022-07-11 12:39:29 -0300

    test: ensure update note throws note not found if note is not found

--- src/domain/use-cases/update-note-title.ts
+++ src/domain/use-cases/update-note-title.ts
@@ -1,10 +1,12 @@
 import { LoadNoteByIdRepository } from '@/domain/contracts'
+import { NoteNotFoundError } from '@/domain/errors'
 
 type Setup = (loadNoteByIdRepository: LoadNoteByIdRepository) => UpdateNoteTitle
 type Input = { noteId: string, newTitle: string }
 export type UpdateNoteTitle = (input: Input) => Promise<void>
 export const setupUpdateNoteTitle: Setup = (loadNoteByIdRepository) => {
   return async ({ noteId }) => {
-    await loadNoteByIdRepository.loadById({ id: noteId })
+    const note = await loadNoteByIdRepository.loadById({ id: noteId })
+    if (note === undefined) throw new NoteNotFoundError()
   }
 }
--- tests/domain/usecases/update-note-title.spec.ts
+++ tests/domain/usecases/update-note-title.spec.ts
@@ -1,17 +1,24 @@
+import { mock, MockProxy } from 'jest-mock-extended'
+
 import { setupUpdateNoteTitle, UpdateNoteTitle } from '@/domain/use-cases/update-note-title'
 import { LoadNoteByIdRepository } from '@/domain/contracts'
-import { mock, MockProxy } from 'jest-mock-extended'
+import { NoteNotFoundError } from '@/domain/errors'
 
 describe('Update Note Title', () => {
   let sut: UpdateNoteTitle
   let loadNoteByIdRepository: MockProxy<LoadNoteByIdRepository>
   let noteId: string
   let newTitle: string
+  let content: string
+  let title: string
 
   beforeAll(() => {
-    loadNoteByIdRepository = mock()
     noteId = 'any_id'
-    newTitle = 'any_title'
+    newTitle = 'new_title'
+    content = 'any_content'
+    title = 'old_title'
+    loadNoteByIdRepository = mock()
+    loadNoteByIdRepository.loadById.mockResolvedValue({ id: noteId, content, title })
   })
 
   beforeEach(() => {
@@ -23,7 +30,14 @@ describe('Update Note Title', () => {
 
     expect(loadNoteByIdRepository.loadById).toHaveBeenCalledWith({ id: noteId })
   })
-  it('should throw NoteNotFound Error if loadNoteById returns undefined', async () => {})
+
+  it('should throw NoteNotFound Error if loadNoteById returns undefined', async () => {
+    loadNoteByIdRepository.loadById.mockResolvedValueOnce(undefined)
+
+    const promise = sut({ noteId, newTitle })
+
+    await expect(promise).rejects.toThrow(new NoteNotFoundError())
+  })
   it('should rethrow if loadNoteById throws', async () => {})
   it('should call updateNoteTitleByIdRepository with correct id and title', async () => {})
   it('should rethrow if updateNoteTitleByIdRepository throws', async () => {})
